38a809f2a6c0174ad5959ee6894bb8804deba56e
Ralph Miole <blackspade2001@gmail.com>
2024-11-14 15:20:55 +0800

    apply templates ofr admin adn user

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -1,147 +1,201 @@
-<?php 
-    //to display errors
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+<?php include("../components/admin-header.php"); ?>
 
-    //database connection
-    include("../connection.php");
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../css/dashboard.css">
-    <title>Voting MS | Admin Dashboard Page</title>
-</head>
+<!-- Main Content -->
+<div id="content">
 
-<body>
+    <!-- Topbar -->
+    <?php include("../components/topbar.php"); ?>
+    <!-- End of Topbar -->
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <a href="admin_dashboard.php" class="logo">
-            <i class='bx bx-code-alt'></i>
-            <div class="logo-name"><span>Voting</span>MS</div>
-        </a>
-        <ul class="side-menu">
-            <li class="active"><a href="admin_dashboard.php"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
-            <li><a href="pending_applicants.php"><i class='bx bx-analyse'></i>Pending Applicants</a></li>
-            <li><a href="approved_applicants.php"><i class='bx bx-group'></i>Approved Applicants</a></li>
-            <li><a href="job_list.php"><i class='bx bx-message-square-dots'></i>Job Lists</a></li>
-            <li><a href="user_logs.php"><i class='bx bx-group'></i>Applicant Logs</a></li>
-            <li><a href="admin_logs.php"><i class='bx bx-group'></i>My Logs</a></li>
-        </ul>
-        <ul class="side-menu">
-            <li>
-                <a href="#" class="logout" id="logoutLink">
-                    <i class='bx bx-log-out-circle'></i>
-                    Logout
-                </a>
-            </li>
-        </ul>
-    </div>
-    <!-- End of Sidebar -->
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
-    <!-- Main Content -->
-    <div class="content">
-        <!-- Navbar -->
-        <nav>
-            <i class='bx bx-menu'></i>
-            
-            <a href="#" class="profile">
-                <img src="images/logo.png">
-            </a>
-        </nav>
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
 
-        <!-- End of Navbar -->
+        <!-- Content Row -->
+        <div class="row">
 
-        <main>
-            <div class="header">
-                <div class="left">
-                    <h1>Dashboard</h1>
-                    <ul class="breadcrumb">
-                        <li><a href="#">Dashboard</a></li>
-                        /
-                        <li><a href="#" class="active">Analytics</a></li>
-                    </ul>
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Earnings (Monthly)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <!-- Insights -->
-            <ul class="insights">
-                <li>
-                    <i class='bx bx-calendar-check'></i>
-                    <span class="info">
-                        <?php
-                        // $query = "SELECT * FROM peso_accounts WHERE userType = 'peso_user' ";
-                        // $run_query = mysqli_query($connForAccounts, $query);
-                        // $row = mysqli_num_rows($run_query);
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Earnings (Annual)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                        // echo '<h3>' . $row . '</h3>'
-                        ?>
-                        <p>Registered Accounts</p>
-                    </span>
-                </li>
-                <li><i class='bx bx-show-alt'></i>
-                    <span class="info">
-                        <?php
-                        // $query = "SELECT * FROM applicants WHERE status = 'pending' ";
-                        // $run_query = mysqli_query($connForPosts, $query);
-                        // $row = mysqli_num_rows($run_query);
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                </div>
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="progress progress-sm mr-2">
+                                            <div class="progress-bar bg-info" role="progressbar"
+                                                style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                                aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                        // echo '<h3>' . $row . '</h3>'
-                        ?>
-                        <p>Pending Applicants</p>
-                    </span>
-                </li>
-                <li><i class='bx bx-line-chart'></i>
-                    <span class="info">
-                        <?php
-                        // $query = "SELECT * FROM applicants WHERE status = 'approved' ";
-                        // $run_query = mysqli_query($connForPosts, $query);
-                        // $row = mysqli_num_rows($run_query);
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                        // echo '<h3>' . $row . '</h3>'
-                        ?>
-                        <p>Approved Applicants</p>
-                    </span>
-                </li>
-                <!-- <li><i class='bx bx-dollar-circle'></i>
-                    <span class="info">
-                        <h3>$6,742</h3>
-                        <p>Total Sales</p>
-                    </span>
-                </li> -->
-            </ul>
-            <!-- End of Insights -->
-
-        </main>
+        <!-- DataTable -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                            </tr>
+                            <tr>
+                                <td>Garrett Winters</td>
+                                <td>Accountant</td>
+                                <td>Tokyo</td>
+                                <td>63</td>
+                                <td>2011/07/25</td>
+                                <td>$170,750</td>
+                            </tr>
+                            <tr>
+                                <td>Shad Decker</td>
+                                <td>Regional Director</td>
+                                <td>Edinburgh</td>
+                                <td>51</td>
+                                <td>2008/11/13</td>
+                                <td>$183,000</td>
+                            </tr>
+                            <tr>
+                                <td>Michael Bruce</td>
+                                <td>Javascript Developer</td>
+                                <td>Singapore</td>
+                                <td>29</td>
+                                <td>2011/06/27</td>
+                                <td>$183,000</td>
+                            </tr>
+                            <tr>
+                                <td>Donna Snider</td>
+                                <td>Customer Support</td>
+                                <td>New York</td>
+                                <td>27</td>
+                                <td>2011/01/25</td>
+                                <td>$112,000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
     </div>
-    
-    <script>
-        // SweetAlert2 confirmation for logout
-        $('#logoutLink').click(function(e) {
-            e.preventDefault(); // Prevent default link behavior
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You will be logged out!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#3085d6',
-                confirmButtonText: 'Yes, logout!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = '../logout.php'; // Redirect to logout page
-                }
-            });
-        });
-    </script>
+    <!-- /.container-fluid -->
 
-    <script src="../js/dashboard.js"></script>
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<?php include("../components/footer.php"); ?>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<?php include("../components/scripts.php"); ?>
+
 </body>
 
 </html>
