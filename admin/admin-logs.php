399e33a55f1cc4c2ebddd4f86a9e7b495cfca54c
Ralph Miole <blackspade2001@gmail.com>
2024-11-22 14:55:44 +0800

    template

--- admin/admin-logs.php
+++ admin/admin-logs.php
@@ -39,11 +39,9 @@
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         </div>
-
-
-
-                <!-- DataTable -->
-                <div class="card shadow mb-4">
+        
+        <!-- DataTable -->
+        <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
             </div>
